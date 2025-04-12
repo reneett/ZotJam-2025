82620b0c30fb1e7fbec5539d0d1a4da42ef4e660
Renee Tran <reneetran2002@hotmail.com>
2025-04-11 23:51:23 -0700

    bounciong

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    
+    [SerializeField] public float jumpHeight;
+    [SerializeField] private float speed;
+
+    private Rigidbody2D body;
+    private bool onUmbrella = false;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        body.linearVelocity = new Vector2(Input.GetAxis("Horizontal")*speed, body.linearVelocity.y);
+        if (onUmbrella) {
+            bounce();
+        }
+    }
+
+    private void bounce() {
+        body.linearVelocity = new Vector2(body.linearVelocity.x, jumpHeight);
+        onUmbrella = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.tag == "Umbrella") {
+            onUmbrella = true;
+        }
+    }
+
+}
