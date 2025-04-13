fd7a9cc218b81f895f0cba1a56b04b29abc6a2c5
Renee Tran <reneetran2002@hotmail.com>
2025-04-13 00:48:52 -0700

    Implemented moving umbrella

---
+++ Assets/Scripts/MoverScript.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class moverScript : MonoBehaviour
+{
+    [Tooltip("How fast the object will move.")]
+    [SerializeField] public float movementSpeed;
+
+    public GameObject movingThing;
+    public Transform startPosition;
+    public Transform endPosition;
+    private int direction = 1;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Transform target = startPosition;
+        if (direction == 1) {
+            movingThing.transform.position = Vector2.MoveTowards(movingThing.transform.position, startPosition.position, movementSpeed*Time.deltaTime);
+        } else {
+            target = endPosition;
+            movingThing.transform.position = Vector2.MoveTowards(movingThing.transform.position, endPosition.position, movementSpeed*Time.deltaTime);
+        }
+            
+        if (Vector2.Distance(movingThing.transform.position, target.position) < 0.01f) {
+            direction *= -1;
+        }
+    
+    }
+}
